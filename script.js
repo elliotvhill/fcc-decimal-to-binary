d4eea2980ad6c7fa410bd95cc6cd241ee2cda70b
Elliot <ellthrowaway2@icloud.com>
2024-06-06 18:04:00 -0400

    refactored decimalToBinary with recursion

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
 const decimalToBinary = (input) => {
-    let binary = '';
-    if (input === 0) {
-        binary = "0"
+    // let binary = '';
+    // if (input === 0) {
+    //     binary = "0"
+    // }
+    // while (input > 0) {
+    //     binary = (input % 2) + binary
+    //     input = Math.floor(input / 2)
+    // }
+    // result.innerText = binary;
+
+    if (input === 0) { // base case
+        return "0"
+    } else { // recursive case
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2)
     }
-    while (input > 0) {
-        binary = (input % 2) + binary
-        input = Math.floor(input / 2)
-    }
-    result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -23,7 +29,7 @@ const checkUserInput = () => {
         alert('Please provide a decimal number greater than or equal to 0');
         return;
     }
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = '';
 };
 
@@ -35,14 +41,14 @@ numberInput.addEventListener('keydown', (e) => {
 });
 
 // recursion example
-const countDownAndUp = (number) => {
-    console.log(number)
-    if (number === 0) { // base case
-        console.log("Reached base case")
-        return
-    } else {
-        countDownAndUp(number - 1)
-        console.log(number)
-    }
-}
-countDownAndUp(3)
+// const countDownAndUp = (number) => {
+//     console.log(number)
+//     if (number === 0) { // base case
+//         console.log("Reached base case")
+//         return
+//     } else { // recursive case
+//         countDownAndUp(number - 1)
+//         console.log(number)
+//     }
+// }
+// countDownAndUp(3)
