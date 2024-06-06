2ef56550914b8cb87d42e1b396d0b0af97c8a939
Elliot <ellthrowaway2@icloud.com>
2024-06-06 17:17:54 -0400

    simplified converter code

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-const numberInput = document.getElementById("number-input")
-const convertBtn = document.getElementById("convert-btn")
-const result = document.getElementById("result")
+const numberInput = document.getElementById('number-input');
+const convertBtn = document.getElementById('convert-btn');
+const result = document.getElementById('result');
 
 const decimalToBinary = (input) => {
-    const inputs = []
-    const quotients = []
-    const remainders = []
-
+    let binary = '';
     if (input === 0) {
-        result.innerText = "0"
-        return
+        binary = "0"
     }
-    
     while (input > 0) {
-        const quotient = Math.floor(input / 2)
-        const remainder = input % 2
-        inputs.push(input)
-        quotients.push(quotient)
-        remainders.push(remainder)
-        input = quotient
+        binary = (input % 2) + binary
+        input = Math.floor(input / 2)
     }
-    console.log("Inputs: ", inputs)
-    console.log("Quotients: ", quotients)
-    console.log("Remainders: ", inputs)
-    result.innerText = remainders.reverse().join("")
-}
+    result.innerText = binary;
+};
 
 const checkUserInput = () => {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
-        alert("Please provide a decimal number greater than or equal to 0")
-        return
+    if (
+        !numberInput.value ||
+        isNaN(parseInt(numberInput.value)) ||
+        parseInt(numberInput.value) < 0
+    ) {
+        alert('Please provide a decimal number greater than or equal to 0');
+        return;
     }
-    decimalToBinary(parseInt(numberInput.value))
-    numberInput.value = ""
-}
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = '';
+};
 
-convertBtn.addEventListener("click", checkUserInput)
-numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        checkUserInput()
+convertBtn.addEventListener('click', checkUserInput);
+numberInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        checkUserInput();
     }
-})
+});
