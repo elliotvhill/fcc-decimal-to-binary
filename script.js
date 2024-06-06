591686712bad4989ca4512447855e9a3f7117536
Elliot <ellthrowaway2@icloud.com>
2024-06-06 17:00:44 -0400

    converter is functional

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const numberInput = document.getElementById("number-input")
 const convertBtn = document.getElementById("convert-btn")
 const result = document.getElementById("result")
 
+const decimalToBinary = (input) => {
+    const inputs = []
+    const quotients = []
+    const remainders = []
+
+    if (input === 0) {
+        result.innerText = "0"
+        return
+    }
+    
+    while (input > 0) {
+        const quotient = Math.floor(input / 2)
+        const remainder = input % 2
+        inputs.push(input)
+        quotients.push(quotient)
+        remainders.push(remainder)
+        input = quotient
+    }
+    console.log("Inputs: ", inputs)
+    console.log("Quotients: ", quotients)
+    console.log("Remainders: ", inputs)
+    result.innerText = remainders.reverse().join("")
+}
+
 const checkUserInput = () => {
     if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
         alert("Please provide a decimal number greater than or equal to 0")
@@ -17,9 +41,3 @@ numberInput.addEventListener("keydown", (e) => {
         checkUserInput()
     }
 })
-
-const decimalToBinary = (input) => {
-    const inputs = []
-    const quotients = []
-    const remainders = []
-}
