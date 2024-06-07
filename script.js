0750e6479c328602dd82bbb06818077c4e3e0946
Elliot <ellthrowaway2@icloud.com>
2024-06-07 16:25:43 -0400

    progress building out animation logic

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
+const animationContainer = document.getElementById('animation-container');
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000,
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500,
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000,
+    },
+];
 
 const decimalToBinary = (input) => {
-    // let binary = '';
-    // if (input === 0) {
-    //     binary = "0"
-    // }
-    // while (input > 0) {
-    //     binary = (input % 2) + binary
-    //     input = Math.floor(input / 2)
-    // }
-    // result.innerText = binary;
-
     if (input === 0 || input === 1) {
         // base case
         return String(input);
@@ -22,25 +30,35 @@ const decimalToBinary = (input) => {
     }
 };
 
-const showAnimation = () => {}
+const showAnimation = () => {
+    result.innerText = 'Call Stack Animation';
+    animationData.forEach((obj) => {
+        setTimeout(() => {
+            animationContainer.innerHTML += `
+            <p
+                id="${obj.inputVal}" 
+                style="margin-top:${obj.marginTop}px;" 
+                class="animation-frame"
+            ></p>
+            `;
+        }, obj.addElDelay);
+    });
+};
 
 const checkUserInput = () => {
-    if (
-        !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0
-    ) {
+    const inputInt = parseInt(numberInput.value);
+    if (!inputInt || isNaN(inputInt) || inputInt < 0) {
         alert('Please provide a decimal number greater than or equal to 0');
         return;
     }
 
     // animation trigger
-    if (parseInt(numberInput.value) === 5) {
-        showAnimation()
-        return
+    if (inputInt === 5) {
+        showAnimation();
+        return;
     }
-    
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = '';
 };
 
@@ -50,4 +68,3 @@ numberInput.addEventListener('keydown', (e) => {
         checkUserInput();
     }
 });
-
